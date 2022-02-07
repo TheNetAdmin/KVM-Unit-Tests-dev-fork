c8fbca79a78ccc59005a06e2adfeb67b7d55a416
Zixuan Wang <zxwang42@gmail.com>
2022-02-06 23:38:04 -0800

    x86 UEFI: read envs from file
    From: Zixuan Wang <zxwang42@gmail.com>
    
    This commit enables kvm-unit-tests to get envs from the host through
    UEFI.
    
    Previously, when kvm-unit-tests runs with Multiboot, it gets a kernel
    binary through QEMU's '-kernel' argument, and envs through the '-initrd'
    argument, which passes a file from host to QEMU, and further stored in
    Multiboot's mbi_bootinfo section.
    
    But under UEFI, kvm-unit-tests loads an EFI binary from a disk image
    instead of QEMU's '-kernel' argument. In this case, QEMU rejects the
    '-initrd' argument without the '-kernel' argument being used, which
    makes kvm-unit-tests unable to get an initrd through this argument.
    
    This commit enables the host to pass the initrd through a file:
      1. The host runner script 'x86/efi/run' captures the '-initrd'
         argument and suppress it from QEMU arguments, so QEMU will not
         report the error
      2. The runner script reads the default initrd and the user-specified
         initrd to generate an 'ENVS.TXT' file for each EFI test case
      3. The kvm-unit-tests EFI set up process reads this 'ENVS.TXT' from
         the disk image and pass it to 'setup_env()'
    
    This commit copies a set of EFI definitions from Linux
    (drivers/firmware/efi/libstub/efistub.h), and implements a set of helper
    functions to read file data through EFI services.
    
    Signed-off-by: Zixuan Wang <zxwang42@gmail.com>

--- lib/linux/efi.h
+++ lib/linux/efi.h
@@ -1,5 +1,9 @@
 /* SPDX-License-Identifier: GPL-2.0 */
-/* Relevant definitions from linux/efi.h. */
+/* 
+ * Relevant definitions from
+ *   - linux/efi.h
+ *   - drivers/firmware/efi/libstub/efistub.h
+ */
 
 #ifndef __LINUX_UEFI_H
 #define __LINUX_UEFI_H
@@ -58,7 +62,10 @@ typedef guid_t efi_guid_t;
 	(b) & 0xff, ((b) >> 8) & 0xff,						\
 	(c) & 0xff, ((c) >> 8) & 0xff, d } }
 
-#define ACPI_TABLE_GUID EFI_GUID(0xeb9d2d30, 0x2d88, 0x11d3, 0x9a, 0x16, 0x00, 0x90, 0x27, 0x3f, 0xc1, 0x4d)
+#define ACPI_TABLE_GUID			EFI_GUID(0xeb9d2d30, 0x2d88, 0x11d3, 0x9a, 0x16, 0x00, 0x90, 0x27, 0x3f, 0xc1, 0x4d)
+#define LOADED_IMAGE_PROTOCOL_GUID	EFI_GUID(0x5b1b31a1, 0x9562, 0x11d2, 0x8e, 0x3f, 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b)
+#define EFI_FILE_SYSTEM_GUID		EFI_GUID(0x964e5b22, 0x6459, 0x11d2, 0x8e, 0x39, 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b)
+#define EFI_FILE_INFO_ID		EFI_GUID(0x09576e92, 0x6d3f, 0x11d2,  0x8e, 0x39, 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b)
 
 typedef struct {
 	efi_guid_t guid;
@@ -414,6 +421,77 @@ struct efi_boot_memmap {
 	u32                     *desc_ver;
 	unsigned long           *key_ptr;
 	unsigned long           *buff_size;
+};
+
+#define __aligned_u64 u64 __attribute__((aligned(8)))
+typedef struct {
+	u32			revision;
+	efi_handle_t		parent_handle;
+	efi_system_table_t	*system_table;
+	efi_handle_t		device_handle;
+	void			*file_path;
+	void			*reserved;
+	u32			load_options_size;
+	void			*load_options;
+	void			*image_base;
+	__aligned_u64		image_size;
+	unsigned int		image_code_type;
+	unsigned int		image_data_type;
+	efi_status_t		(__efiapi *unload)(efi_handle_t image_handle);
+} efi_loaded_image_t;
+
+typedef struct {
+	u64			size;
+	u64			file_size;
+	u64			phys_size;
+	efi_time_t		create_time;
+	efi_time_t		last_access_time;
+	efi_time_t		modification_time;
+	__aligned_u64		attribute;
+	efi_char16_t		filename[];
+} efi_file_info_t;
+
+typedef struct efi_file_protocol efi_file_protocol_t;
+
+struct efi_file_protocol {
+	u64		revision;
+	efi_status_t	(__efiapi *open)	(efi_file_protocol_t *,
+						 efi_file_protocol_t **,
+						 efi_char16_t *, u64, u64);
+	efi_status_t	(__efiapi *close)	(efi_file_protocol_t *);
+	efi_status_t	(__efiapi *delete)	(efi_file_protocol_t *);
+	efi_status_t	(__efiapi *read)	(efi_file_protocol_t *,
+						 unsigned long *, void *);
+	efi_status_t	(__efiapi *write)	(efi_file_protocol_t *,
+						 unsigned long, void *);
+	efi_status_t	(__efiapi *get_position)(efi_file_protocol_t *, u64 *);
+	efi_status_t	(__efiapi *set_position)(efi_file_protocol_t *, u64);
+	efi_status_t	(__efiapi *get_info)	(efi_file_protocol_t *,
+						 efi_guid_t *, unsigned long *,
+						 void *);
+	efi_status_t	(__efiapi *set_info)	(efi_file_protocol_t *,
+						 efi_guid_t *, unsigned long,
+						 void *);
+	efi_status_t	(__efiapi *flush)	(efi_file_protocol_t *);
+};
+
+typedef struct efi_simple_file_system_protocol efi_simple_file_system_protocol_t;
+
+struct efi_simple_file_system_protocol {
+	u64	revision;
+	int	(__efiapi *open_volume)(efi_simple_file_system_protocol_t *,
+					efi_file_protocol_t **);
+};
+
+#define EFI_FILE_MODE_READ	0x0000000000000001
+#define EFI_FILE_MODE_WRITE	0x0000000000000002
+#define EFI_FILE_MODE_CREATE	0x8000000000000000
+
+#define MAX_FILENAME_SIZE	256
+
+struct finfo {
+	efi_file_info_t info;
+	efi_char16_t	filename[MAX_FILENAME_SIZE];
 };
 
 #define efi_bs_call(func, ...) efi_system_table->boottime->func(__VA_ARGS__)
